73212ca695f72673930ccedaf5798ae55f6899d8
Ant Cosentino <antony@mycosentino.com>
2015-05-01 16:15:04 +0200

    added explicit default for status.node.load in template

--- app/routes/apps.js
+++ app/routes/apps.js
@@ -3,11 +3,13 @@ var http = require('http');
 
 var path = process.env.NODE_APPS_JSON || 'http://data.goddard.com:8080/apps.json';
 
+var blank = { apps: [] };
+
 module.exports = function(app) {
   app.all(
     process.env.NODE_APPS_ROUTE || '/',
     function(req, res) {
-      var apps = '';
+      
       if (req.method === 'POST' || req.method === 'post') {
         process.emit(
           'log:access',
@@ -20,13 +22,16 @@ module.exports = function(app) {
         );
       }
       http.get(path, function(httpres) {
+        var apps = '';
         httpres.on('data', function(data) {
           apps += data;
         }).on('end', function() {
           process.nextTick(function() {
-            res.render('apps', {apps: JSON.parse(apps || '{}')});
+            if (apps === '' || apps === '{}') res.render('apps', {apps: blank.apps});
+            else res.render('apps', {apps: JSON.parse(apps)});
           });
         }).on('error', function(err) {
+          console.error('apps.js http error', err);
           res.status(500).end(err);
         });
       });
