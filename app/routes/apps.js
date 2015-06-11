805fff1f8da42331fbc9af8d77ba75057744aa1e
Johann du Toit <exxonno@gmail.com>
2015-06-11 12:54:54 +0200

    Updated to support the actual logging of MAC addresses correctly only once when it hits the portal from redirect

--- app/routes/apps.js
+++ app/routes/apps.js
@@ -43,11 +43,7 @@ module.exports = function(app) {
     process.env.NODE_APPS_ROUTE || '/',
     function(req, res) {
       // redirect to mamawifi.com if still on goddard.com
-      if(req.hostname.toLowerCase().indexOf('mamawifi') === -1 && 1==2) {
-        
-        res.redirect('http://mamawifi.com');
-
-      } else {
+      if(req.hostname.toLowerCase().indexOf('mamawifi') === -1) {
 
         if (req.method === 'POST' || req.method === 'post') {
           process.emit(
@@ -60,48 +56,43 @@ module.exports = function(app) {
             ]
           );
         }
+        
+        res.redirect('http://mamawifi.com');
 
-        // check for redirect
-        if( (req.hostname || '').toLowerCase().indexOf('goddard.com') !== -1 ) {
+      } else {
 
-          res.redirect('http://mamawifi.com');
-
-        } else {
-
-          http.get(path, function(httpres) {
-            var apps = '';
-            httpres.on('data', function(data) {
-              apps += data;
-            }).on('end', function() {
-              process.nextTick(function() {
-                if (httpres.statusCode > 200) {
-                  return res.render('apps', {
-                    apps: blank.app,
-                    whitelist: blank.whitelist,
-                    error: {message: 'connect ECONNREFUSED'}
-                  });
-                } else if (apps === '' || apps === '{}') {
-                  return res.render('apps', {
-                    apps: blank.apps,
-                    whitelist: blank.whitelist
-                  });
-                } else {
-                  return res.render('apps', {
-                    apps: JSON.parse(apps),
-                    whitelist: blank.whitelist
-                  });
-                }
-              });
-            });
-          }).on('error', function(err) {
-            return res.render('apps', {
-              apps: blank.apps,
-              whitelist: blank.whitelist,
-              error: {message: 'connect ECONNREFUSED'}
+        http.get(path, function(httpres) {
+          var apps = '';
+          httpres.on('data', function(data) {
+            apps += data;
+          }).on('end', function() {
+            process.nextTick(function() {
+              if (httpres.statusCode > 200) {
+                return res.render('apps', {
+                  apps: blank.app,
+                  whitelist: blank.whitelist,
+                  error: {message: 'connect ECONNREFUSED'}
+                });
+              } else if (apps === '' || apps === '{}') {
+                return res.render('apps', {
+                  apps: blank.apps,
+                  whitelist: blank.whitelist
+                });
+              } else {
+                return res.render('apps', {
+                  apps: JSON.parse(apps),
+                  whitelist: blank.whitelist
+                });
+              }
             });
           });
-
-        }
+        }).on('error', function(err) {
+          return res.render('apps', {
+            apps: blank.apps,
+            whitelist: blank.whitelist,
+            error: {message: 'connect ECONNREFUSED'}
+          });
+        });
 
       }
     }
