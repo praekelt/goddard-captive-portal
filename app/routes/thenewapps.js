5f826a2dacaecea9e6d23b60d5d8c90928b52101
Ant Cosentino <antony@mycosentino.com>
2015-11-03 13:26:33 +0200

    initial commit of head request runner

--- app/routes/thenewapps.js
+++ app/routes/thenewapps.js
@@ -1,9 +1,51 @@
 
 'use strict';
 
+var url = require('url');
+var http = require('http');
+
+var thenewappsPath = __dirname + '/../../test/fixtures/thenewapps.json';
+var media = url.parse(process.env.NODE_HOST_MEDIA || 'http://127.0.0.1:8080/media');
 var path = process.env.NODE_THENEWAPPS_JSON || 'http://127.0.0.1:8080/thenewapps.json';
-var thenewapps = require('../../test/fixtures/thenewapps.json');
+var thenewapps = require(thenewappsPath);
 var route = process.env.NODE_THENEWAPPS_ROUTE || '/thenewapps';
+
+var heads = 0, finished = 0;
+
+function rewriteManifest() {
+  fs.unlink(thenewappsPath, function(err) {
+    if (err) return done(err);
+    fs.writeFile(thenewappsPath, JSON.stringify(thenewapps), function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+}
+
+function checkMediaAvailability() {
+  this.get('thenewapps.content.dontHaveCategories').forEach(function(category, i) {
+    category.media.forEach(function(medium, k) {
+      http.request({
+        hostname: media.hostname,
+        path: [media.path, category.uri, medium.uri].join('/'),
+        method: 'head'
+      }, function(res) {
+        process.emit('head:response', i, k, res.headers['content-length']);
+      }).end();
+    });
+  });
+  this.get('thenewapps.content.haveCategories').forEach(function(category, i) {
+    category.media.forEach(function(medium, k) {
+      http.request({
+        hostname: media.hostname,
+        path: [media.path, category.uri, medium.uri].join('/'),
+        method: 'head'
+      }, function(res) {
+        process.emit('head:response', i, k, res.headers['content-length']);
+      }).end();
+    });
+  });
+}
 
 function collate(manifest) {
   this.set('thenewapps.content.menu', manifest.categories.map(function(category) {
@@ -54,14 +96,33 @@ function registerChildCategory(category, parentUri) {
   });
 }
 
-module.exports = function(app) {
-  collate.call(app, thenewapps);
-  registerAllParentCategories.call(app);
-  registerAllTopLevelCategories.call(app);
-  app.all(route, function(req, res) {
+function init(manifest) {
+  collate.call(this, manifest);
+  registerAllParentCategories.call(this);
+  registerAllTopLevelCategories.call(this);
+  this.all(route, function(req, res) {
     res.render('thenewapps_home', {
       category: {name: 'mamaconnect', uri: route},
-      menu: app.get('thenewapps.content.menu')
+      menu: this.get('thenewapps.content.menu')
+    });
+  }.bind(this));
+}
+
+module.exports = function(app) {
+  process.on('head:response', function(categoryIndex, mediaIndex, size) {
+    var medium = thenewapps.categories[categoryIndex].media[mediaIndex];
+    medium.available = size >= medium.size;
+    if (++finished !== heads) return;
+    heads = 0, finished = 0;
+    rewriteManifest(function(err) {
+      if (err) console.log(err);
+      thenewapps = require(thenewappsPath);
+      init.call(app, thenewapps);
     });
   });
+
+  init.call(app, thenewapps);
+
+  // ready to test once we have a host web server to use
+  // setInterval(checkMediaAvailability.bind(app), 90000);
 };
