88fb6f3063e57a6780447cc4c55fe3f42f29c815
Ant Cosentino <antony@mycosentino.com>
2015-11-03 20:21:59 +0200

    removed process listener and counters implementation new implementation uses async *shudder* this commit contains some kind of a looping error (not necessarily using a literal loop) the rewritemanifest function seems to execute endlessly (continuously deleting and rewriting a json file)

--- app/routes/thenewapps.js
+++ app/routes/thenewapps.js
@@ -1,8 +1,10 @@
 
 'use strict';
 
+var fs = require('fs');
 var url = require('url');
 var http = require('http');
+var async = require('async');
 
 var thenewappsPath = __dirname + '/../../test/fixtures/thenewapps.json';
 var media = url.parse(process.env.NODE_HOST_MEDIA || 'http://127.0.0.1:8080/media');
@@ -10,53 +12,52 @@ var path = process.env.NODE_THENEWAPPS_JSON || 'http://127.0.0.1:8080/thenewapps
 var thenewapps = require(thenewappsPath);
 var route = process.env.NODE_THENEWAPPS_ROUTE || '/thenewapps';
 
-var heads = 0, finished = 0;
-
 function rewriteManifest() {
   fs.unlink(thenewappsPath, function(err) {
-    if (err) return done(err);
-    fs.writeFile(thenewappsPath, JSON.stringify(thenewapps), function(err) {
-      if (err) return done(err);
-      done();
+    if (err) return console.log(err);
+    fs.writeFile(thenewappsPath, JSON.stringify(thenewapps, null, '  '), function(err) {
+      if (err) console.log(err);
+      else process.emit('init');
     });
   });
 }
 
 function checkMediaAvailability() {
-  var requests = [];
-  this.get('thenewapps.content.dontHaveCategories').forEach(function(category, i) {
-    category.media.forEach(function(medium, k) {
-      heads += 1;
-      requests.push(http.request({
-        hostname: media.hostname,
-        path: [media.path, category.uri, medium.uri].join('/'),
-        method: 'head'
-      }, function(res) {
-        process.emit('head:response', i, k, res.headers['content-length']);
-      }));
+  function head(done) {
+    return http.request({
+      hostname: media.hostname,
+      path: [media.path, this.uri].join('/'),
+      method: 'head'
+    }, function(res) {
+      this.available = parseInt(
+        res.headers['content-length'], 10
+      ) >= this.size;
+      done(null, this.available);
+    }.bind(this)).on('error', done.bind(done)).end();
+  }
+  var headRequests = [];
+  thenewapps.categories.forEach(function(category) {
+    (category.media || []).forEach(function(medium) {
+      headRequests.push(head.bind(medium));
+    });
+    (category.categories || []).forEach(function(category) {
+      category.media.forEach(function(medium) {
+        headRequests.push(head.bind(medium));
+      });
     });
   });
-  this.get('thenewapps.content.haveCategories').forEach(function(category, i) {
-    category.media.forEach(function(medium, k) {
-      heads += 1;
-      requests.push(http.request({
-        hostname: media.hostname,
-        path: [media.path, category.uri, medium.uri].join('/'),
-        method: 'head'
-      }, function(res) {
-        process.emit('head:response', i, k, res.headers['content-length']);
-      }));
-    });
-  });
-
-  requests.forEach(function(request) {
-    request.end();
+  async.parallel(headRequests, function(err, results) {
+    if (err) console.log('error:', err);
+    else rewriteManifest();
   });
 }
 
 function collate(manifest) {
   this.set('thenewapps.content.menu', manifest.categories.map(function(category) {
-    return {name: category.name, uri: route + '/' + category.uri};
+    return {
+      name: category.name,
+      uri: route + '/' + category.uri
+    };
   }));
   this.set('thenewapps.content.haveCategories', manifest.categories.filter(function(category) {
     return !!category.categories;
@@ -116,20 +117,14 @@ function init(manifest) {
 }
 
 module.exports = function(app) {
-  process.on('head:response', function(categoryIndex, mediaIndex, size) {
-    var medium = thenewapps.categories[categoryIndex].media[mediaIndex];
-    medium.available = size >= medium.size;
-    if (++finished !== heads) return;
-    heads = 0, finished = 0;
-    rewriteManifest(function(err) {
-      if (err) console.log(err);
-      thenewapps = require(thenewappsPath);
-      init.call(app, thenewapps);
-    });
+  process.on('init', function() {
+    thenewapps = require(thenewappsPath);
+    init.call(app, thenewapps);
   });
+  //
+  // queue this up to run every fifteen minutes
+  setInterval(checkMediaAvailability.bind(app), 90000);
 
-  init.call(app, thenewapps);
-
-  // ready to test once we have a host web server to use
-  // setInterval(checkMediaAvailability.bind(app), 90000);
+  // run it once, immediately
+  checkMediaAvailability();
 };
