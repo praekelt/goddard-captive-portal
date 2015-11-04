387fa4a8d78f80af91b9b2171405ceedfab3cc6c
Ant Cosentino <antony@mycosentino.com>
2015-11-04 12:11:11 +0200

    base test cases for new portal pages

--- test/index.js
+++ test/index.js
@@ -246,5 +246,46 @@ describe('app', function() {
         });
       });
     });
+
+    describe('/thenewapps', function() {
+      describe('allowed methods', function() {
+        it('should respond to get', function() {
+          var request = chakram.get('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to post', function() {
+          var request = chakram.post('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to put', function() {
+          var request = chakram.put('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to patch', function() {
+          var request = chakram.patch('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to options', function() {
+          var request = chakram.options('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to head', function() {
+          var request = chakram.head('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+        it('should respond to delete', function() {
+          var request = chakram.delete('http://localhost:4321/thenewapps');
+          chakram.expect(request).to.have.status(200);
+          return chakram.wait();
+        });
+      });
+
+    });
   });
 });
