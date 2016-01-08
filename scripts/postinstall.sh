d23a63b9b06cc1d140bf884b5ff66207802a1276
Bruce McIntyre <bruce.mcintyre@gmail.com>
2016-01-08 14:41:24 +0200

    Checks to see if link exists.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,4 +4,6 @@ cd /var/praekelt/goddard-captive-portal
 npm install
 
 mkdir -p /var/goddard/apps
-ln -s /var/praekelt/goddard-captive-portal /var/goddard/apps/captiveportal-redesign
+if [ ! -L /var/goddard/apps/captiveportal-redesign ]; then
+  ln -s /var/praekelt/goddard-captive-portal /var/goddard/apps/captiveportal-redesign
+fi
