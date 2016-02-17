37d484fdd67d0d4a6132af6998fb78559a634251
Ant Cosentino <antony@mycosentino.com>
2016-02-17 22:55:50 +0200

    reordered variables added logging fixed up morgan

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 
 'use strict';
 
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+
+var async = require('async');
+var express = require('express');
+var morgan = require('morgan');
+
 var boot = require('./app/boot');
-var env = process.env.NODE_ENV || 'dev';
-var path = require('path'), fs = require('fs'), os = require('os');
-var async = require('async'), express = require('express');
-var port = process.env.NODE_PORT || process.env.PORT || 3000;
-var paths = {
-  log: path.join(__dirname, 'access.log'),
-  views: path.join(__dirname, 'app/views'),
-  static: path.join(__dirname, 'app/static')
-}, access = fs.createWriteStream(paths.log);
+
+var PORT = process.env.NODE_PORT || process.env.PORT || 3000;
+var NODE_ENV = process.env.NODE_ENV || 'dev';
+var NODE_TEST_FIXTURES = process.env.NODE_TEST_FIXTURES || false;
+var GODDARD_ACCESS_LOG_PATH = process.env.GODDARD_ACCESS_LOG_PATH || path.join(__dirname, 'access.log');
+var GODDARD_APP_STATIC_PATH = process.env.GODDARD_APP_STATIC_PATH || path.join(__dirname, 'app/static');
+var GODDARD_APP_VIEWS_PATH = process.env.GODDARD_APP_VIEWS_PATH || path.join(__dirname, 'app/views');
+
+var access = fs.createWriteStream(GODDARD_ACCESS_LOG_PATH, {flags: 'a'});
 
 process.on('log:access', function(line) {
-  access.write(line.join('\t') + os.EOL);
+  access.write(line.join('\t') + '\n');
 }).on('log:delete', function() {
   access.end(function() {
-    fs.unlink(paths.log, function(err) {
+    fs.unlink(GODDARD_ACCESS_LOG_PATH, function(err) {
       if (err) console.error('HTTP DELETE > unlink access.log:', err);
-      access = fs.createWriteStream(paths.log);
+      access = fs.createWriteStream(GODDARD_ACCESS_LOG_PATH, {flags: 'a'});
     });
   });
 }).on('console:log', console.log.bind(console));
 
 var app = express();
 
-env === 'dev' || env.indexOf('test') > -1 ? app.use(require('morgan')('dev')) : 0;
-app.set('paths', paths);
-app.set('views', paths.views);
+app.set('paths', {
+  log: GODDARD_ACCESS_LOG_PATH,
+  views: GODDARD_APP_VIEWS_PATH,
+  static: GODDARD_APP_STATIC_PATH
+});
+app.set('views', GODDARD_APP_VIEWS_PATH);
 app.set('view engine', 'jade');
-app.use('/static', express.static(paths.static));
+if (NODE_ENV === 'dev') app.use(morgan(NODE_ENV));
+app.use('/static', express.static(GODDARD_APP_STATIC_PATH));
 app.use(require('body-parser').urlencoded({extended: true}));
 
-if (process.env.NODE_TEST_FIXTURES) {
+if (NODE_TEST_FIXTURES) {
   boot.fixtures(function() {
     boot.apps(function(err) {
       if (err) {
         console.log(err);
         process.exit(1);
       }
+      console.log('TEST SUITE - BE PATIENT!');
       console.log('apps.json already present, continuing...');
       require('./app/routes')(app);
-      app.listen(port, function() {
-        console.log("✔ server listening at localhost:%s in %s mode...", port, env);
+      app.listen(PORT, function() {
+        console.log("✔ server listening at localhost:%s in %s mode...", PORT, NODE_ENV);
         module.exports = app;
       });
     });
@@ -55,8 +68,8 @@ if (process.env.NODE_TEST_FIXTURES) {
     }
     console.log('apps.json already present, continuing...');
     require('./app/routes')(app);
-    app.listen(port, function() {
-      console.log("✔ server listening at localhost:%s in %s mode...", port, env);
+    app.listen(PORT, function() {
+      console.log("✔ server listening at localhost:%s in %s mode...", PORT, NODE_ENV);
       module.exports = app;
     });
   });
