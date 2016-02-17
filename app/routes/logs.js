71a93e9ccd196c4d5e46ae51d9aab2892a52b717
Ant Cosentino <antony@mycosentino.com>
2016-02-17 22:50:33 +0200

    readability

--- app/routes/logs.js
+++ app/routes/logs.js
@@ -7,8 +7,8 @@ var GODDARD_LOG_ROUTE = process.env.GODDARD_LOG_ROUTE || '/log';
 
 module.exports = function(app) {
   app.get(GODDARD_LOG_ROUTE, function(req, res) {
-    res.set('Content-type', 'text/plain');
     res.status(200);
+    res.set('Content-type', 'text/plain');
     fs.createReadStream(app.get('paths').log).pipe(res);
   });
   app.delete('/log', function(req, res) {
