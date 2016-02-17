f760ca43c7482f4dd1469059c0682aaa96942351
Ant Cosentino <antony@mycosentino.com>
2016-02-17 14:02:19 +0200

    fixed up error handling per subsystem

--- app/routes/status.js
+++ app/routes/status.js
@@ -7,7 +7,8 @@ var http = require('http'),
     nodePath = process.env.NODE_NODE_JSON || 'http://127.0.0.1:8080/node.json',
     buildPath = process.env.NODE_BUILD_JSON || 'http://127.0.0.1:8080/build.json',
     wifiStatusPath = process.env.NODE_WIFI_PAGE || 'http://127.0.0.1:8080/wireless.html',
-    mediaSyncLog = process.env.NODE_MEDIA_SYNC_LOG || 'http://127.0.0.1:8080/media_sync.log';
+    mediaSyncLog = process.env.NODE_MEDIA_SYNC_LOG || 'http://127.0.0.1:8080/media_sync.log',
+    mediaSizesLog = process.env.NODE_MEDIA_SIZES_LOG || 'http://127.0.0.1:8080/media_sizes.log';
 
 var blank = {
   build: {
@@ -63,22 +64,72 @@ var blank = {
     },
     "nodeid": "0",
     "timestamp": 10
+  },
+  errors: {
+    build: [],
+    node: [],
+    status: [],
+    mediaSizes: [],
+    mediaSync: [],
+    wificheck: [],
   }
 };
 
 module.exports = function(app) {
   app.get(process.env.NODE_STATUS_ROUTE || '/status', function(req, res) {
+
+    // reset the error lists for every request
+    blank.errors.build = [];
+    blank.errors.node = [];
+    blank.errors.status = [];
+    blank.errors.mediaSizes = [];
+    blank.errors.mediaSync = [];
+    blank.errors.wificheck = [];
+
     async.parallel({
+      mediaSizes: function(mediaSizesCallback) {
+        http.get(mediaSizesLog, function(httpres) {
+          var response = '';
+          httpres.on('data', function(data) {
+            response += data;
+          }).on('end', function() {
+            if (response === '') {
+              blank.errors.mediaSizes.push(
+                'no disk usage data was found in the media disk usage log!'
+              );
+              return mediaSizesCallback();
+            }
+            mediaSizesCallback(null, response);
+          });
+        }).on('error', function(err) {
+          blank.errors.mediaSizes.push(err);
+          mediaSizesCallback();
+        });
+      },
       mediaSync: function(mediaSyncCallback) {
         http.get(mediaSyncLog, function(httpres) {
           var response = '';
           httpres.on('data', function(data) {
             response += data;
           }).on('end', function() {
-            mediaSyncCallback(null, response);
+            if (response === '') {
+              blank.errors.mediaSync.push(
+                'the rsync log file is empty. ' +
+                'do not panic! ' +
+                'it is likely that rsync will start soon and create a new log file.'
+              );
+              return mediaSyncCallback();
+            }
+            mediaSyncCallback(null, (
+              'Total files: '       + /.* (\d+) files to consider/igm.exec(response)[1] + '\n' +
+              'Bytes transmitted: ' + /.* sent (\d+) bytes/igm.exec(response)[1]        + '\n' +
+              'Bytes received: '    + /.* received (\d+) bytes/igm.exec(response)[1]    + '\n' +
+              'Bytes total: '       + /.* total size is (\d+)/igm.exec(response)[1]
+            ));
           });
         }).on('error', function(err) {
-          mediaSyncCallback(err);
+          blank.errors.mediaSync.push(err);
+          mediaSyncCallback();
         });
       },
       wificheck: function(wificheckCallback) {
@@ -86,13 +137,14 @@ module.exports = function(app) {
           httpres.on('data', function(data) {
           }).on('end', function() {
             wificheckCallback(null, httpres.statusCode >= 200 && httpres.statusCode < 400);
-          }).on('socket', function (socket) {
+          }).on('socket', function(socket) {
             socket.setTimeout(1500);
             socket.on('timeout', function() {
               httpres.abort();
             });
           });
         }).on('error', function(err) {
+          blank.errors.wificheck.push(err);
           wificheckCallback(null, false);
         });
       },
@@ -103,15 +155,23 @@ module.exports = function(app) {
             response += data;
           }).on('end', function() {
             if (httpres.statusCode > 200) {
+              blank.errors.status.push('expected http response code to be `200`, but found it to be `' + httpres.statusCode + '`');
               statusCallback(null, blank.status);
             } else if (response === '' || response === '{}') {
+              blank.errors.status.push('expected a full json response but received `' + response + '`');
               statusCallback(null, blank.status);
             } else {
-              statusCallback(null, JSON.parse(response));
+              try {
+                statusCallback(null, JSON.parse(response));
+              } catch (err) {
+                blank.errors.status.push(err);
+                statusCallback(null, blank.status);
+              }
             }
           });
         }).on('error', function(err) {
-          statusCallback(err, null);
+          blank.errors.status.push(err);
+          statusCallback(null, blank.status);
         });
       },
       node: function(nodeCallback) {
@@ -121,15 +181,23 @@ module.exports = function(app) {
             response += data;
           }).on('end', function() {
             if (httpres.statusCode > 200) {
+              blank.errors.node.push('expected http response code to be `200`, but found it to be `' + httpres.statusCode + '`');
               nodeCallback(null, blank.node);
             } else if (response === '' || response === '{}') {
+              blank.errors.node.push('expected a full json response but received `' + response + '`');
               nodeCallback(null, blank.node);
             } else {
-              nodeCallback(null, JSON.parse(response));
+              try {
+                nodeCallback(null, JSON.parse(response));
+              } catch (err) {
+                blank.errors.node.push(err);
+                nodeCallback(null, blank.node);
+              }
             }
           });
         }).on('error', function(err) {
-          nodeCallback(err, null);
+          blank.errors.node.push(err);
+          nodeCallback(null, blank.node);
         });
       },
       build: function(buildCallback) {
@@ -139,40 +207,35 @@ module.exports = function(app) {
             response += data;
           }).on('end', function() {
             if (httpres.statusCode > 200) {
+              blank.errors.build.push('expected http response code to be `200`, but found it to be `' + httpres.statusCode + '`');
               buildCallback(null, blank.build);
             } else if (response === '' || response === '{}') {
+              blank.errors.build.push('expected a full json response but received `' + response + '`');
               buildCallback(null, blank.build);
             } else {
               try {
                 buildCallback(null, JSON.parse(response));
-              } catch(err) {
+              } catch (err) {
+                blank.errors.build.push(err);
                 buildCallback(null, blank.build);
               }
             }
           });
         }).on('error', function(err) {
-          buildCallback(err, null);
+          blank.errors.build.push(err);
+          buildCallback(null, blank.build);
         });
       }
     }, function(err, results) {
-      if (err) {
-        return res.render('status', {
-          status: blank.status,
-          node: blank.node,
-          build: blank.build,
-          wifiavailable: false,
-          mediaSync: false,
-          error: err
-        });
-      } else {
-        return res.render('status', {
-          status: JSON.stringify(results.status) == '{}' ? blank.status : results.status,
-          node: results.node,
-          mediaSync: results.mediaSync,
-          wifiavailable: results.wificheck,
-          build: results.build
-        });
-      }
+      return res.render('status', {
+        errors: blank.errors,
+        status: results.status,
+        node: results.node,
+        mediaSync: results.mediaSync,
+        mediaSizes: results.mediaSizes,
+        wificheck: results.wificheck,
+        build: results.build
+      });
     });
   });
 };
